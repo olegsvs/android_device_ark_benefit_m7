1447522e32ead368cdddcf1e80a163fd6a18bb5b
olegsvs <oleg.texet@gmail.com>
2016-06-29 08:54:09 -0400

    Update for thermal & snap disable

--- patches/apply-patch.sh
+++ patches/apply-patch.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 cd ../../../..
 cd frameworks/av
-git apply -v ../../device/ark/benefit_m7/patches/framework_av/0001-Patch-for-6735-mtk.patch
+git apply --check -v ../../device/ark/benefit_m7/patches/framework_av/0001-Patch-for-6735-mtk.patch
 cd ../..
 cd frameworks/base
-git apply -v ../../device/ark/benefit_m7/patches/framework_base/0001-XenonHD-up.patch
-git apply -v ../../device/ark/benefit_m7/patches/framework_base/0001-Fix-typo.patch
+git apply --check -v ../../device/ark/benefit_m7/patches/framework_base/0001-XenonHD-up.patch
+git apply --check -v ../../device/ark/benefit_m7/patches/framework_base/0001-Fix-typo.patch
 cd ../..
 cd packages/apps/Settings/
-git apply -v ../../../device/ark/benefit_m7/patches/packages_apps_Settings/0001-Patch-for-6735-mtk.patch
+git apply --check -v ../../../device/ark/benefit_m7/patches/packages_apps_Settings/0001-Patch-for-6735-mtk.patch
 cd ../../..
 cd frameworks/opt/telephony
-git apply -v ../../../device/ark/benefit_m7/patches/framework_opt_telephony/0001-XenonHD-up.patch
+git apply --check -v ../../../device/ark/benefit_m7/patches/framework_opt_telephony/0001-XenonHD-up.patch
 cd ../../..
 cd hardware/libhardware
-git apply -v ../../device/ark/benefit_m7/patches/hardware_libhardware/0001-Patch-for-6735-mtk.patch
+git apply --check -v ../../device/ark/benefit_m7/patches/hardware_libhardware/0001-Patch-for-6735-mtk.patch
 cd ../..
 cd hardware/libhardware_legacy
-git apply -v ../../device/ark/benefit_m7/patches/hardware_libhardware_legacy/0001-Patch-for-6735-mtk.patch
+git apply --check -v ../../device/ark/benefit_m7/patches/hardware_libhardware_legacy/0001-Patch-for-6735-mtk.patch
 cd ../..
 cd packages/services/Telephony
-git apply -v ../../../device/ark/benefit_m7/patches/packages_services_telephony/0001-Patch-for-6735-mtk.patch
+git apply --check -v ../../../device/ark/benefit_m7/patches/packages_services_telephony/0001-Patch-for-6735-mtk.patch
 cd ../../..
 cd system/core
-git apply -v ../../device/ark/benefit_m7/patches/system_core/0001-XenonHD-up.patch
+git apply --check -v ../../device/ark/benefit_m7/patches/system_core/0001-XenonHD-up.patch
 cd ../..
 cd system/netd
-git apply -v ../../device/ark/benefit_m7/patches/system_netd/0001-Patch-for-6735-mtk.patch
+git apply --check -v ../../device/ark/benefit_m7/patches/system_netd/0001-Patch-for-6735-mtk.patch
 cd ../..
 echo Patches Applied Successfully!
