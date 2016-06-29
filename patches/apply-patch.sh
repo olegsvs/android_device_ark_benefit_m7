d3d51190714ed53aceb478d5b0eba039610e7e87
olegsvs <oleg.texet@gmail.com>
2016-06-29 09:41:48 -0400

    update

--- patches/apply-patch.sh
+++ patches/apply-patch.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 cd ../../../..
 cd frameworks/av
-git apply -v ../../device/ark/benefit_m7/patches/framework_av/0001-Patch-for-6735-mtk.patch
+git apply -v ../../device/ark/benefit_m7/patches/framework_av/0001-mt6735.patch
 cd ../..
 cd frameworks/base
-git apply -v ../../device/ark/benefit_m7/patches/framework_base/0001-Patch-for-6735-mtk.patch
+git apply -v ../../device/ark/benefit_m7/patches/framework_base/0001-mt6735.patch
 cd ../..
 cd packages/apps/Settings/
 git apply -v ../../../device/ark/benefit_m7/patches/packages_apps_Settings/0001-Patch-for-6735-mtk.patch
 cd ../../..
 cd frameworks/opt/telephony
-git apply -v ../../../device/ark/benefit_m7/patches/framework_opt_telephony/0001-Patch-for-6735-mtk.patch
+git apply -v ../../../device/ark/benefit_m7/patches/framework_opt_telephony/0001-mt6735.patch
 cd ../../..
 cd hardware/libhardware
 git apply -v ../../device/ark/benefit_m7/patches/hardware_libhardware/0001-Patch-for-6735-mtk.patch
@@ -21,9 +21,9 @@ cd ../..
 cd packages/services/Telephony
 git apply -v ../../../device/ark/benefit_m7/patches/packages_services_telephony/0001-Patch-for-6735-mtk.patch
 cd ../../..
-#cd system/core
-#git apply -v ../../device/ark/benefit_m7/patches/system_core/0001-Patch-for-6735-mtk.patch
-#cd ../..
+cd system/core
+git apply -v ../../device/ark/benefit_m7/patches/system_core/0001-mt6735.patch
+cd ../..
 cd system/netd
 git apply -v ../../device/ark/benefit_m7/patches/system_netd/0001-Patch-for-6735-mtk.patch
 cd ../..
